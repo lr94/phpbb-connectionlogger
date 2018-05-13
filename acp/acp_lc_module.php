8b7fc022519b34c74a2f5eda51e5eba9520969a4
lr94 <lucarobbiano@gmail.com>
2018-05-13 13:35:59 +0200

    Indent array definitions

--- acp/acp_lc_module.php
+++ acp/acp_lc_module.php
@@ -93,9 +93,30 @@ class acp_lc_module
 			}
 
 			// Sorting
-			$limit_days = array(0 => $user->lang('ALL_ENTRIES'), 1 => $user->lang('1_DAY'), 7 => $user->lang('7_DAYS'), 14 => $user->lang('2_WEEKS'), 30 => $user->lang('1_MONTH'), 90 => $user->lang('3_MONTHS'), 180 => $user->lang('6_MONTHS'), 365 => $user->lang('1_YEAR'));
-			$sort_by_text = array('u' => $user->lang('SORT_USERNAME'), 't' => $user->lang('SORT_DATE'), 'i' => $user->lang('SORT_IP'), 'o' => $user->lang('SORT_ACTION'));
-			$sort_by_sql = array('u' => 'u.username_clean', 't' => 'l.log_time', 'i' => 'l.log_ip', 'o' => 'l.log_operation');
+			$limit_days = array(
+				0	=> $user->lang('ALL_ENTRIES'),
+				1	=> $user->lang('1_DAY'),
+				7	=> $user->lang('7_DAYS'),
+				14	=> $user->lang('2_WEEKS'),
+				30	=> $user->lang('1_MONTH'),
+				90	=> $user->lang('3_MONTHS'),
+				180	=> $user->lang('6_MONTHS'),
+				365	=> $user->lang('1_YEAR')
+			);
+
+			$sort_by_text = array(
+				'u' => $user->lang('SORT_USERNAME'),
+				't' => $user->lang('SORT_DATE'),
+				'i' => $user->lang('SORT_IP'),
+				'o' => $user->lang('SORT_ACTION')
+			);
+
+			$sort_by_sql = array(
+				'u' => 'u.username_clean',
+				't' => 'l.log_time',
+				'i' => 'l.log_ip',
+				'o' => 'l.log_operation'
+			);
 
 			$s_limit_days = $s_sort_key = $s_sort_dir = $u_sort_param = '';
 			gen_sort_selects($limit_days, $sort_by_text, $sort_days, $sort_key, $sort_dir, $s_limit_days, $s_sort_key, $s_sort_dir, $u_sort_param);
